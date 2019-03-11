5ca0f654f01c329db650c4ec8df3e24842468732
rosesharondi@icloud.com <rosesharondi@icloud.com>
2019-03-11 18:27:46 -0500

    Code added

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,20 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            Console.WriteLine("Enter a date (MM/DD/YYYY): ");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter another date (MM/DD/YYYY): ");
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
 
+            DaysBetween(date1, date2);
             Console.ReadLine();
         }
-
+        public static double DaysBetween(DateTime userDT1, DateTime userDT2)
+        {
+            double daysBetween = (userDT2 - userDT1).TotalDays;           
+            return daysBetween;            
+        }
         // TODO: Create that has a method that counts the number of days between two different dates.
-    }
+    }   
 }
--- Test/ProgramTest.cs
+++ Test/ProgramTest.cs
@@ -9,13 +9,15 @@ namespace Test
         [Fact]
         public void DaysBetween_After()
         {
-            Assert.Equal(2, Program.DaysBetween(DateTime.Parse("1/4/2018"), DateTime.Parse("1/6/2018")));
+            Assert.Equal(2, Program.DaysBetween(DateTime.Parse("1/4/2018"), 
+                DateTime.Parse("1/6/2018")));
         }
 
         [Fact]
         public void DaysBetween_Before()
         {
-            Assert.Equal(2, Program.DaysBetween(DateTime.Parse("1/6/2018"), DateTime.Parse("1/4/2018")));
+            Assert.Equal(2, Program.DaysBetween(DateTime.Parse("1/6/2018"), 
+                DateTime.Parse("1/4/2018")));
         }
     }
 }
